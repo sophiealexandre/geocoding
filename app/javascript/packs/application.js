511c9dda75785d1b83eb0a95e2fa984225bb1a29
sophie alexandre <sophie.alec@gmail.com>
2020-01-31 10:35:41 -0500

    Initial commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,28 @@
 import "bootstrap";
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+
+initMapbox();
+initAutocomplete();
+
+markers.forEach((marker) => {
+
+  const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+  // Create a HTML element for your custom marker
+  const element = document.createElement('div');
+  element.className = 'marker';
+  element.style.backgroundImage = `url('${marker.image_url}')`;
+  element.style.backgroundSize = 'contain';
+  element.style.width = '25px';
+  element.style.height = '25px';
+
+  // Pass the element as an argument to the new marker
+  new mapboxgl.Marker(element)
+    .setLngLat([marker.lng, marker.lat])
+    .setPopup(popup)
+    .addTo(map);
+});
